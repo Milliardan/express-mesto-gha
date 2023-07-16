131f5981ce4d585603b82339697ff81e4eefb08d
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-07-16 20:33:54 +0200

    fix:иправления к ревью 1

--- utils/constants.js
+++ utils/constants.js
@@ -4,9 +4,10 @@ const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
 function handleError(err, req, res) {
+  console.error(err);
   if (err.constructor.name === 'CastError') {
     res.status(BAD_REQUEST).send({
-      message: 'Неверный формат переданных данных',
+      message: err.message,
     });
     return;
   }
@@ -20,13 +21,13 @@ function handleError(err, req, res) {
 
   if (err.constructor.name === 'NotFoundError') {
     res.status(NOT_FOUND).send({
-      message: 'Пользователь не найден',
+      message: err.message,
     });
     return;
   }
 
   res.status(INTERNAL_SERVER_ERROR).send({
-    message: 'Произошла ошибка на сервере',
+    message: err.message,
   });
 }
 
