80bacc514e3560bf7db528528f7dcab3f7069d27
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-07-16 15:01:28 +0200

    feat:реализовать логику работы сервера

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { routes } = require('./routes');
+
+const { PORT = 3000 } = process.env;
+const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
+
+const app = express();
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log(`Connected to database on ${DATABASE_URL}`);
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
+
+// временное решение авторизации пользователя
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64b3e17052f9c861c39b0fba',
+  };
+
+  next();
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
