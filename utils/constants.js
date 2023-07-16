ab77b0e1d3bf0990b0da16be04889f9fecb5eb7c
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-07-16 21:23:01 +0200

    fix:иправления к ревью 1

--- utils/constants.js
+++ utils/constants.js
@@ -4,9 +4,8 @@ const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
 function handleError(err, req, res) {
-  console.error(err);
   if (err.constructor.name === 'CastError') {
-    res.status(BAD_REQUEST).send({
+    res.status(NOT_FOUND).send({
       message: err.message,
     });
     return;
@@ -26,6 +25,7 @@ function handleError(err, req, res) {
     return;
   }
 
+  console.error(err);
   res.status(INTERNAL_SERVER_ERROR).send({
     message: err.message,
   });
