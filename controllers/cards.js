3972df9a839b3221fded2b3f387d4ae3d15c6f8d
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-07-16 19:51:19 +0200

    fix:иправления к ревью 1

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,7 +6,7 @@ async function createCard(req, res) {
     const { name, link } = req.body;
     const ownerId = req.user._id;
     const card = await Card.create({ name, link, owner: ownerId });
-    res.send(card);
+    res.status(HTTP_CREATED).send(card);
   } catch (err) {
     handleError(err, req, res);
   }
