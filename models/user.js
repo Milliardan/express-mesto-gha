2c71293637dbd06045604dbda317a40061d5a735
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-07-28 18:17:08 +0200

    feat: Модуль Validation, валидация  email при создании пользователя

--- models/user.js
+++ models/user.js
@@ -22,6 +22,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      validate: {
+        validator: validator.isEmail,
+      },
     },
     password: {
       type: String,
